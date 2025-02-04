7295c2f9ad43faa5325e93ddca59745ab1d85b0d
Mahmoud Elbanna <elbanna.mahmoud.a@gmail.com>
2025-02-04 21:57:16 +0100

    Refactor LanguageView to use StatelessWidget and simplify language selection

--- lib/features/account/presentation/widgets/language_view.dart
+++ lib/features/account/presentation/widgets/language_view.dart
@@ -3,62 +3,35 @@ import 'package:flutter_bloc/flutter_bloc.dart';
 
 import '../../../../hotel_booking_app.dart';
 
-class LanguageView extends StatefulWidget {
+class LanguageView extends StatelessWidget {
   const LanguageView({super.key});
 
   @override
-  State<LanguageView> createState() => _LanguageViewState();
-}
-
-class _LanguageViewState extends State<LanguageView> {
-  late final List<String> options;
-  late String selectedLanguage;
-
-  @override
-  void initState() {
-    super.initState();
-    options = [
-      AppLocalizations.supportedLocales[0].languageCode,
-      AppLocalizations.supportedLocales[1].languageCode
-    ];
-    selectedLanguage = options[0];
-  }
-
-  @override
   Widget build(BuildContext context) {
+    final languageCode =
+        context.watch<LanguageCubit>().state.languageCode ?? '';
+    final germanLocale = AppLocalizations.supportedLocales[0];
+    final englishLocale = AppLocalizations.supportedLocales[1];
+
     return Padding(
       padding: const EdgeInsets.all(16.0),
       child: Column(
-        crossAxisAlignment: CrossAxisAlignment.start,
         spacing: 20,
+        crossAxisAlignment: CrossAxisAlignment.start,
         children: [
           LanguageListTile(
             text: context.l10n.german,
-            value: options[0],
-            groupValue: selectedLanguage,
-            onChanged: (value) {
-              context.read<LanguageCubit>().languageSelected(
-                  AppLocalizations.supportedLocales[0].languageCode, 'DE');
-              setState(
-                () {
-                  selectedLanguage = value.toString();
-                },
-              );
-            },
+            value: germanLocale.languageCode,
+            groupValue: languageCode,
+            onChanged: (value) =>
+                context.read<LanguageCubit>().languageSelected(value, 'DE'),
           ),
           LanguageListTile(
             text: context.l10n.english,
-            value: options[1],
-            groupValue: selectedLanguage,
-            onChanged: (value) {
-              context.read<LanguageCubit>().languageSelected(
-                  AppLocalizations.supportedLocales[1].languageCode, 'US');
-              setState(
-                () {
-                  selectedLanguage = value.toString();
-                },
-              );
-            },
+            value: englishLocale.languageCode,
+            groupValue: languageCode,
+            onChanged: (value) =>
+                context.read<LanguageCubit>().languageSelected(value, 'US'),
           ),
         ],
       ),
