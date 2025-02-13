8b0e5bb82616209f76366ccb2f7c16a9b01f9cef
Mahmoud Elbanna <elbanna.mahmoud.a@gmail.com>
2025-02-13 21:49:57 +0100

    update flutter_gen to false

--- test/features/hotels/presentation/pages/hotels_page_test.dart
+++ test/features/hotels/presentation/pages/hotels_page_test.dart
@@ -4,9 +4,9 @@ import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:get_it/get_it.dart';
-import 'package:mockito/annotations.dart';
-import 'package:hotel_booking_app/hotel_booking_app.dart';
 import 'package:mockito/mockito.dart';
+
+import 'package:hotel_booking_app/hotel_booking_app.dart';
 
 import '../../../../fixtures/test_mocks.mocks.dart';
 
